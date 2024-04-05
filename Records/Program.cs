18fa7cf5f7b9e8e0d5aa9777e87a6e6cfd48e777
Alejandro Campano <a.campano.galan@avanade.com>
2024-04-05 13:03:02 +0200

    Records base

---
+++ Records/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
